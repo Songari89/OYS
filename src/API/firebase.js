9463810de05ce277ca246ef334c84a2dde97fde0
Ari <ari89.web@gmail.com>
2024-01-04 22:53:25 +0900

    FIREBASE LOGIN

---
+++ src/API/firebase.js
@@ -0,0 +1,32 @@
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+const auth = getAuth();
+
+
+export function login() {
+  signInWithPopup(auth, provider)
+  .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    const user = result.user;
+    console.log(user)
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    const email = error.customData.email;
+    const credential = GoogleAuthProvider.credentialFromError(error);
+  });
+
+}
+
--- src/components/AuthPopup.jsx
+++ src/components/AuthPopup.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "./AuthPopup.module.css";
+import { login } from "../API/firebase";
 
 export default function AuthPopup() {
   return (
     <div className={styles.authcontainer}>
       <p>로그인하고 더 많은 혜택을 받으세요!</p>
-      <button>로그인</button>
+      <button onClick={login}>로그인</button>
     </div>
   );
 }
